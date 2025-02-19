2ddc220732a9e404f303a6b80e64c7a4648c0bf5
james_p <jamesphilipbp@gmail.com>
2025-02-19 06:50:34 -0800

    Production

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -1,5 +1,6 @@
 class AdminDashboardController < ApplicationController
-  before_action :authenticate_user!  # Ensures users are logged in
+  before_action :authenticate_user!
+  #before_action :authorize_admin  # This calls the method below
 
   def index
     @users = User.all
@@ -18,5 +19,9 @@ class AdminDashboardController < ApplicationController
 
     redirect_to admin_dashboard_path
   end
+
+ # private  # Ensure this is here
+
+  #
 end
 
